30be925168fcf4f672606a084e448c7ec7894e27
gabo <gabrieliturrab@ug.uchile.cl>
2017-05-16 19:54:31 -0400

    macadores con info window, falta hacer el link a la informacion de las fotos

--- Tarea3/getComunas.php
+++ Tarea3/getComunas.php
@@ -6,13 +6,13 @@ require_once('consults.php');
 
 $db = DbConfig::getConnection() or die("No se ha podido conectar: " . pg_last_error());
 
-$query = "SELECT C.nombre, F.titulo FROM `comuna` as C, fotografia as F WHERE C.id = F.comuna";
+$query = "SELECT C.nombre as comuna, F.titulo as titulo, P.nombre as participante, F.participante as id FROM `comuna` as C, fotografia as F, participante as P WHERE C.id = F.comuna AND P.id = F.participante";
 $db->prepare($query);
 $result = $db->query($query);
 $db->close(); 
 $out = [];
 while ($row = $result->fetch_assoc()) {
-	$out[$row['nombre']][] = $row;
+	$out[$row['comuna']][] = $row;
 }
 
 echo json_encode($out,JSON_PRETTY_PRINT);
